be2f44af8fd966bf1a8fd6555b4eadd781dda216
LionCking700 <Hellckingder2000@gmail.com>
2026-02-18 20:34:41 -0600

    18Feb

--- Assets/Scripts/PlayerCollide.cs
+++ Assets/Scripts/PlayerCollide.cs
@@ -4,8 +4,11 @@ using UnityEngine.Events;
 public class PlayerCollide : MonoBehaviour
 {
     [SerializeField]
-
     private string obstacleTag = "Obstacle";
+    [SerializeField]
+    private string coinTag = "Coin";
+    [SerializeField]
+    private UnityEvent<Transform> onCoinCollected;
     [SerializeField]
     private UnityEvent <Transform> onObstacleCollision;
     private void OnTriggerEnter(Collider other)
@@ -13,6 +16,11 @@ public class PlayerCollide : MonoBehaviour
         if (other.CompareTag(obstacleTag))
         {
             onObstacleCollision?.Invoke(transform);
+        }
+        else if(other.CompareTag(coinTag))
+        {
+            onCoinCollected?.Invoke(transform);
+            other.gameObject.SetActive(false);
         }
     }
 }
